7d8b89e9720f60baf3340a0f158e16e0446d543d
PiPiXia101 <15503733987@163.com>
2019-09-06 17:30:23 +0800

    国外医疗论坛爬虫

--- text.py
+++ text.py
@@ -25,50 +25,48 @@ headers = {
     'X-Requested-With': 'XMLHttpRequest',
     'Connection': 'keep-alive',
 }
+for i in range(1,10):
+    params = (
+        ('p_l_id', '20759'),
+        ('p_p_id', ['scolarissearchresultsportlet_WAR_scolarissearchresults',
+                    'scolarissearchresultsportlet_WAR_scolarissearchresults']),
+        ('p_p_lifecycle', ['0', '0']),
+        ('p_t_lifecycle', '0'),
+        ('p_p_state', ['normal', 'normal']),
+        ('p_p_mode', ['view', 'view']),
+        ('p_p_col_id', ['column-1', 'column-1']),
+        ('p_p_col_pos', '0'),
+        ('p_p_col_count', ['1', '1']),
+        ('p_p_isolated', '1'),
+        ('currentURL', '%2Fweb%2Fcochrane%2Fsearch'),
+        ('min_year', ''),
+        ('max_year', ''),
+        ('custom_min_year', ''),
+        ('custom_max_year', ''),
+        ('searchBy', '6'),
+        ('searchText', '*'),
+        ('selectedType', 'review'),
+        ('isWordVariations', ''),
+        ('resultPerPage', '25'),
+        ('searchType', 'basic'),
+        ('orderBy', 'relevancy'),
+        ('publishDateTo', ''),
+        ('publishDateFrom', ''),
+        ('publishYearTo', ''),
+        ('publishYearFrom', ''),
+        ('displayText', ''),
+        ('forceTypeSelection', 'true'),
+        ('cur', str(i)),
+        ('pathname', '%2Fcdsr%2Freviews'),
+    )
 
-params = (
-    ('p_l_id', '20759'),
-    ('p_p_id', ['scolarissearchresultsportlet_WAR_scolarissearchresults',
-                'scolarissearchresultsportlet_WAR_scolarissearchresults']),
-    ('p_p_lifecycle', ['0', '0']),
-    ('p_t_lifecycle', '0'),
-    ('p_p_state', ['normal', 'normal']),
-    ('p_p_mode', ['view', 'view']),
-    ('p_p_col_id', ['column-1', 'column-1']),
-    ('p_p_col_pos', '0'),
-    ('p_p_col_count', ['1', '1']),
-    ('p_p_isolated', '1'),
-    ('currentURL', '%2Fweb%2Fcochrane%2Fsearch'),
-    ('min_year', ''),
-    ('max_year', ''),
-    ('custom_min_year', ''),
-    ('custom_max_year', ''),
-    ('searchBy', '6'),
-    ('searchText', '*'),
-    ('selectedType', 'review'),
-    ('isWordVariations', ''),
-    ('resultPerPage', '25'),
-    ('searchType', 'basic'),
-    ('orderBy', 'relevancy'),
-    ('publishDateTo', ''),
-    ('publishDateFrom', ''),
-    ('publishYearTo', ''),
-    ('publishYearFrom', ''),
-    ('displayText', ''),
-    ('forceTypeSelection', 'true'),
-    ('cur', '1'),
-    ('pathname', '%2Fcdsr%2Freviews'),
-)
-
-response = requests.get('https://www-cochranelibrary-com.rpa.skh.org.tw/en/c/portal/render_portlet', headers=headers,
-                        params=params, cookies=cookies, verify=False)
-# print(response.text)
-html = response.text
-parse_html = etree.HTML(html)
-tz_list = parse_html.xpath("//div[@class='search-results-item']")
-for tz in tz_list:
-    num = tz.xpath("./div[@class='search-results-item-tools']/div/label/text()")
-    title = tz.xpath("./div[@class='search-results-item-body']/h3[@class='result-title']/a/text()")
-    content = tz.xpath(
-        "./div[@class='search-results-item-body']/div[@class='search-result-authors']/div/text()")
-    print(num, title, '\n')
+    response = requests.get('https://www-cochranelibrary-com.rpa.skh.org.tw/en/c/portal/render_portlet', headers=headers,params=params, cookies=cookies, verify=False)
+    html = response.text
+    parse_html = etree.HTML(html)
+    tz_list = parse_html.xpath("//div[@class='search-results-item']")
+    for tz in tz_list:
+        num = tz.xpath("./div[@class='search-results-item-tools']/div/label/text()")
+        title = tz.xpath("./div[@class='search-results-item-body']/h3[@class='result-title']/a/text()")
+        content = tz.xpath(
+            "./div[@class='search-results-item-body']/div[@class='search-result-authors']/div/text()")
+        print(num, title, '\n')
